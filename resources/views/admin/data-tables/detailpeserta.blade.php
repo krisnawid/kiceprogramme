a3c878ed250f93d608f22f0b6b9d27e1e61d3811
krisnawid <krisna.widi.kw@gmail.com>
2020-01-14 14:59:06 +0700

    disable click modal ketika proses

--- resources/views/admin/data-tables/detailpeserta.blade.php
+++ resources/views/admin/data-tables/detailpeserta.blade.php
@@ -109,7 +109,7 @@
           </div>
           <!---Container Fluid-->
 
-          <div class="modal fade" id="modalKonfrimasi" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div class="modal fade" id="modalKonfrimasi" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" data-backdrop="static" data-keyboard="false">
             <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
--- resources/views/user/registration.blade.php
+++ resources/views/user/registration.blade.php
@@ -101,7 +101,7 @@
     </div>
 </section>
 
-<div class="modal fade" id="modalProses" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+<div class="modal fade" id="modalProses" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" data-backdrop="static" data-keyboard="false">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
